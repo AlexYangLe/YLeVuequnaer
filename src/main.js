0be6ed40444f21a7ba8fb8f72cb61faf955d3821
AlexYangLe <yanduhantan563@sina.com>
2018-12-21 00:15:26 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+//移动端开发300ms延时解决
+import fastClick from 'fastclick'
+//所有的手机浏览器正常显示
+import './assets/styles/reset.css'
+//1像素边框解决方案
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
